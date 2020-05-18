0f61fe5d4fc0ed17f2cf4ead0e8eac979e7b806b
Fatih Yılmaz <65228887+Albedo426@users.noreply.github.com>
2020-05-18 15:13:24 +0300

    Add files via upload

---
+++ serialportbaglanti/serialportbaglanti/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace serialportbaglanti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(600, 349);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 402);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "com1 baglan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.com1connet);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "com2baglan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.com2connet);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "gonder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.send_message);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ serialportbaglanti/serialportbaglanti/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace serialportbaglanti
+{
+    public partial class Form1 : Form
+    {
+        byte[] rx_buffer = new byte[256];
+        int rx_buffer_counter = 0;
+        public SerialPort sp1 = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        public String byteConvert(SerialPort s)
+        {
+            int bytes = s.BytesToRead;//gelen byte boyutu
+            byte[] buffer = new byte[bytes];//byte dizisi oluştur
+            s.Read(buffer, 0, bytes);//veri oku yaz
+            MessageBox.Show(buffer[0]+"");
+            MessageBox.Show(buffer[1]+"");
+            MessageBox.Show(buffer[2]+"");
+            byte[] bufferstr = new byte[Convert.ToInt32(buffer[2])-3];//data olan kısım için byte dizisi//
+            int j=0;
+            for (int i=3;i< Convert.ToInt32(buffer[2])-1; i++) {
+                bufferstr[j] = buffer[i];//data olan kısmı yeni byte dizisine atıyoruz
+                j++;
+            }
+            if ((buffer[2]- buffer[buffer[2]-1])/ 13 != 0) {//bozulma olup olmadığı kontrolü//ikide verinin tamamı var tüm boyut sağ kısım da en sonda kalan crc
+                MessageBox.Show("veride bozulma var");
+            }
+            return Encoding.ASCII.GetString(bufferstr);//data return
+        }
+        private void com1connet(object sender, EventArgs e)
+        {
+            connet("1");
+        }
+        public void connet(String s) {
+       
+            sp1.PortName = "COM"+s;
+            sp1.BaudRate = 19200;//ne kadar az olursa veri okadar yavaşlar dalga boyu gibi düşün 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200
+            sp1.DataBits = 8;//göndereceğimiz bilginin kaç bitten oluşacağını bildiriyoruz 8,7
+            sp1.Parity = Parity.Mark;// alınan verinin doğruluğpu için
+            /*
+                None =kontrol yok
+                Odd = bit sayısını tek bir yapar
+                Even = bit sayısını çift bir yapar
+            */
+            sp1.StopBits = StopBits.One;//bitiş bit'i genelde 1 bit yapılıyor 
+            Console.WriteLine("bağlantı com"+s);
+            sp1.Open();
+            sp1.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived1);//listener
+        }
+        private void com2connet(object sender, EventArgs e)
+        {
+            connet("2");
+        }
+        private void sp_DataReceived1(object sender, SerialDataReceivedEventArgs e)
+        {
+
+           int bytes = sp1.BytesToRead;//gelen byte boyutu
+            sp1.Read(rx_buffer, rx_buffer_counter, bytes);//veri oku yaz
+                                                          /*int bytes = s.BytesToRead;//gelen byte boyutu
+                                                          byte[] buffer = new byte[bytes];//byte dizisi oluştur
+                                                          s.Read(buffer, 0, bytes);//veri oku yaz*/
+
+
+            rx_buffer_counter += bytes;
+
+        }
+
+        private void send_message(object sender, EventArgs e)
+        {
+            byte[] bytes = crativepacket();
+            sp1.Write(bytes, 0, bytes.Length);
+        }
+        private byte[] crativepacket() {
+            byte[] bytes = new byte[textBox1.Text.Length+4];
+            bytes[0] = 1;
+            bytes[1] = 2;
+            bytes[2] = Convert.ToByte(textBox1.Text.Length+4);
+            String str = textBox1.Text;
+
+            for(int i=0;i< textBox1.Text.Length;i++)
+                bytes[i+3]= (byte)str[i];
+
+
+            int top=0;
+            for (int i = 0; i < textBox1.Text.Length + 3; i++) {
+                top += bytes[i];
+            }
+            bytes[textBox1.Text.Length+3] = Convert.ToByte(top % 127);
+            
+            return bytes;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (rx_buffer_counter > 3 )
+            {
+                if (rx_buffer_counter >= Convert.ToInt32(rx_buffer[2]))//paket geldi
+                {
+                    int top = 0;
+                    for (int i = 0; i < rx_buffer[2] - 1; i++)
+                    {
+                        top += rx_buffer[i];
+                    }
+                    if ((top - rx_buffer[rx_buffer[2] - 1]) % 127 == 0)
+                    {
+                        byte[] bufferstr = new byte[Convert.ToInt32(rx_buffer[2]) - 4];//data olan kısım için byte dizisi//
+                        for (int i = 0; i < Convert.ToInt32(rx_buffer[2]) - 4; i++)
+                        {
+                            bufferstr[i] = rx_buffer[i + 3];//data olan kısmı yeni byte dizisine atıyoruz
+                        }
+                        richTextBox1.Text += Encoding.ASCII.GetString(bufferstr) + "\n";
+                    }
+                    else {
+                        MessageBox.Show("hatalı veri geldi");
+                    }
+                    rx_buffer_counter = 0;
+
+                }
+
+
+
+                //byte[] bufferstr = new byte[Convert.ToInt32(rx_buffer[2]) - 3];//data olan kısım için byte dizisi//
+                //int j = 0;
+                //for (int i = 3; i < Convert.ToInt32(rx_buffer[2]) - 1; i++)
+                //{
+                //    bufferstr[j] = rx_buffer[i];//data olan kısmı yeni byte dizisine atıyoruz
+                //    j++;
+                //}
+                ///*if ((rx_buffer[2] - rx_buffer[rx_buffer[2] - 1]) % 13 != 0)
+                //{//bozulma olup olmadığı kontrolü//ikide verinin tamamı var tüm boyut sağ kısım da en sonda kalan crc
+                //    MessageBox.Show("veride bozulma var");
+                //}*/
+                //richTextBox1.Text += Encoding.ASCII.GetString(bufferstr) + "\n";
+                //rx_buffer_counter = 0;
+                ///*
+                //    richTextBox1.Text += byteConvert(sp1) + "\n";
+                //}*/
+            }
+        }
+    }
+}
